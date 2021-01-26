d9f7cb2bee90ac1bbf2df5f456ae0a8cc71559fa
Anthony Miller <AnthonyMDev@gmail.com>
2021-01-25 16:18:05 -0800

    Initial Commit

---
+++ Shared/CodegenProposalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodegenProposalApp.swift
+//  Shared
+//
+//  Created by Anthony Miller on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CodegenProposalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
