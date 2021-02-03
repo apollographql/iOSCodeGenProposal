73feaf05ea754040e2efb2049fb52c0f77597a5c
Anthony Miller <AnthonyMDev@gmail.com>
2021-02-02 16:08:52 -0800

    WIP: Add config for unit tests

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -2,8 +2,10 @@
 
 let cat = AllAnimals(__typename: "Cat",
                      species: "Cat",
-                     height: .init(props: .init(__typename: "Height", feet: 2, inches: 10)))
-  .makeAsPet(humanName: "Tiger Lily", favoriteToy: "Shoelaces")
+                     height: .init(props: .init(__typename: "Height", feet: 2, inches: 10, meters: 4)))
+//  .makeAsPet(humanName: "Tiger Lily", favoriteToy: "Shoelaces")
 //  .makeAsWarmBlooded(bodyTemperature: 100)
 
 //let species = cat.species // This code completes all properties and accesses them properly!
+
+cat.height
--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -6,37 +6,24 @@
 //
 
 import XCTest
+@testable import CodegenProposalFramework
 
 class Tests_iOS: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+  }
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+  func testExample() throws {
+    let cat = AllAnimals(__typename: "Cat",
+                         species: "Cat",
+                         height: .init(props: .init(__typename: "Height", feet: 2, inches: 10, meters: 4)))
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    XCTAssertEqual(cat.species, "Cat")
+  }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
