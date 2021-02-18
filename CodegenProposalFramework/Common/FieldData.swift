5a634c7f752cd4e899d304de161bd91722353b8c
Anthony Miller <AnthonyMDev@gmail.com>
2021-02-18 13:00:35 -0800

    Make FieldData a final class

--- CodegenProposalFramework/Common/FieldData.swift
+++ CodegenProposalFramework/Common/FieldData.swift
@@ -8,13 +8,18 @@
 import Foundation
 
 // TODO: Docs
-struct FieldData<Fields, TypeCaseFields> { // TODO: One generic param for ResponseObject type? Try this again after making base classes!
+final class FieldData<Fields, TypeCaseFields> { // TODO: One generic param for ResponseObject type? Try this again after making base classes!
   let fields: Fields
   let typeCaseFields: TypeCaseFields
+
+  init(fields: Fields, typeCaseFields: TypeCaseFields) {
+    self.fields = fields
+    self.typeCaseFields = typeCaseFields
+  }
 }
 
 extension FieldData where TypeCaseFields == Void {
-  init(fields: Fields) {
+  convenience init(fields: Fields) {
     self.init(fields: fields, typeCaseFields: ())
   }
 }
