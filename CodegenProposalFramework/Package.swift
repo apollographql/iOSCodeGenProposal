ac406724d86400c6db4536adbd6aebb547855b69
Anthony Miller <AnthonyMDev@gmail.com>
2021-02-26 15:45:26 -0800

    Added Apollo dependency

--- CodegenProposalFramework/Package.swift
+++ CodegenProposalFramework/Package.swift
@@ -2,16 +2,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "CodegenProposalFramework",
-    products: [
-        .library(name: "CodegenProposalFramework", targets: ["CodegenProposalFramework"]),
-    ],
-    targets: [
-        .target(
-            name: "CodegenProposalFramework",
-            dependencies: []),
-        .testTarget(
-            name: "CodegenProposalFrameworkTests",
-            dependencies: ["CodegenProposalFramework"]),
-    ]
+  name: "CodegenProposalFramework",
+  platforms: [
+    .iOS(.v12),
+    .macOS(.v10_14),
+    .tvOS(.v12),
+    .watchOS(.v5)
+  ],
+  products: [
+    .library(name: "CodegenProposalFramework", targets: ["CodegenProposalFramework"]),
+  ],
+  dependencies: [
+    .package(name: "Apollo",
+             url: "https://github.com/apollographql/apollo-ios.git",
+             .upToNextMinor(from: "0.41.0")),
+  ],
+  targets: [
+    .target(
+      name: "CodegenProposalFramework",
+      dependencies: [
+        .product(name: "Apollo", package: "Apollo"),
+        .product(name: "ApolloCodegenLib", package: "Apollo")
+      ]
+    ),
+    .testTarget(
+      name: "CodegenProposalFrameworkTests",
+      dependencies: ["CodegenProposalFramework"]),
+  ]
 )
