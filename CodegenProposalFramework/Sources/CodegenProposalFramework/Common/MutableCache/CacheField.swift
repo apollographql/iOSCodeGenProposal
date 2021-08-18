cc58cbc78bd25094fa9baab5fdf8936e2c1fce25
Anthony Miller <AnthonyMDev@gmail.com>
2021-08-18 11:23:04 -0700

    Put property wrappers in CacheEntity namespace

--- CodegenProposalFramework/Sources/CodegenProposalFramework/Common/MutableCache/CacheField.swift
+++ CodegenProposalFramework/Sources/CodegenProposalFramework/Common/MutableCache/CacheField.swift
@@ -1,106 +1,42 @@
-@propertyWrapper
-public struct CacheField<T> {
+extension CacheEntity {
+  @propertyWrapper
+  public struct CacheField<T> {
 
-  private let field: StaticString
+    private let field: StaticString
 
-  public init(_ field: StaticString) {
-    self.field = field
-  }
+    public init(_ field: StaticString) {
+      self.field = field
+    }
 
-  public static subscript<E: CacheEntity>(
-    _enclosingInstance instance: E,
-    wrapped wrappedKeyPath: ReferenceWritableKeyPath<E, T?>,
-    storage storageKeyPath: ReferenceWritableKeyPath<E, Self>
-  ) -> T? {
-    get {
-      let field = instance[keyPath: storageKeyPath].field
-      let data = instance.data[field.description]
-
-      if let cacheKey = data as? CacheKey {
-        fatalError()
-//        return CacheReference.entity(instance._transaction.entity(withKey: cacheKey.key)) as? T
-
-      } else if let dataDict = data as? [String: Any] {
-//          let entity = instance._transaction.entity(withData: dataDict)
-//          return CacheReference.entity(entity) as! T // CacheReference<CacheEntity>
-        fatalError()
-
-      } else {
+    public static subscript<E: CacheEntity>(
+      _enclosingInstance instance: E,
+      wrapped wrappedKeyPath: ReferenceWritableKeyPath<E, T?>,
+      storage storageKeyPath: ReferenceWritableKeyPath<E, Self>
+    ) -> T? {
+      get {
+        let field = instance[keyPath: storageKeyPath].field
+        let data = instance.data[field.description]
         return data as? T
       }
-    }
-    set {
-//      let field = instance[keyPath: storageKeyPath].field
-//
-//      switch newValue {
-//      case let reference as AnyCacheReference:
-//        instance.data[field] = reference.cacheData
-//
-//      default:
-//        break // TODO
-////        fatalError() // TODO
-//      }
-    }
-  }
-
-  public var projectedValue: CacheField { self }
-
-  @available(*, unavailable,
-      message: "This property wrapper can only be applied to CacheEntity objects."
-  )
-  public var wrappedValue: T? { get { fatalError() } set { fatalError() } }
-}
-
-
-@propertyWrapper
-public struct CacheRef<R> {
-
-  private let field: StaticString
-
-  public init(_ field: StaticString) {
-    self.field = field
-  }
-
-  public static subscript<E: CacheEntity, R>(
-    _enclosingInstance instance: E,
-    wrapped wrappedKeyPath: ReferenceWritableKeyPath<E, CacheReference<R>?>,
-    storage storageKeyPath: ReferenceWritableKeyPath<E, Self>
-  ) -> CacheReference<R>? {
-    get {
-      let field = instance[keyPath: storageKeyPath].field
-      let data = instance.data[field.description]
-
-      if let cacheKey = data as? CacheKey {
-        fatalError()
-//        return CacheReference.entity(instance._transaction.entity(withKey: cacheKey.key)) as? T
-
-      } else if let dataDict = data as? [String: Any] {
-        let entity: R = instance._transaction.entity(withData: dataDict) as! R
-        return CacheReference.entity(entity)
-//        fatalError()
-
-      } else {
-        fatalError()
+      set {
+        //      let field = instance[keyPath: storageKeyPath].field
+        //
+        //      switch newValue {
+        //      case let reference as AnyCacheReference:
+        //        instance.data[field] = reference.cacheData
+        //
+        //      default:
+        //        break // TODO
+        ////        fatalError() // TODO
+        //      }
       }
     }
-    set {
-//      let field = instance[keyPath: storageKeyPath].field
-//
-//      switch newValue {
-//      case let reference as AnyCacheReference:
-//        instance.data[field] = reference.cacheData
-//
-//      default:
-//        break // TODO
-////        fatalError() // TODO
-//      }
-    }
+
+    public var projectedValue: CacheField { self }
+
+    @available(*, unavailable,
+    message: "This property wrapper can only be applied to CacheEntity objects."
+    )
+    public var wrappedValue: T? { get { fatalError() } set { fatalError() } }
   }
-
-  public var projectedValue: CacheRef { self }
-
-  @available(*, unavailable,
-      message: "This property wrapper can only be applied to CacheEntity objects."
-  )
-  public var wrappedValue: CacheReference<R>? { get { fatalError() } set { fatalError() } }
 }
--- CodegenProposalFramework/Sources/CodegenProposalFramework/Common/MutableCache/CacheReference.swift
+++ CodegenProposalFramework/Sources/CodegenProposalFramework/Common/MutableCache/CacheReference.swift
@@ -78,54 +78,61 @@ public struct CacheKey {
   }
 }
 
-//@propertyWrapper
-//public struct CacheReference<T> {
-//
-//  private let field: String // TODO: Use StaticString?
-//
-//  public init(_ field: String) {
-//    self.field = field
-//  }
-//
-//  public static subscript<E: CacheEntity>(
-//    _enclosingInstance instance: E,
-//    wrapped wrappedKeyPath: ReferenceWritableKeyPath<E, T?>,
-//    storage storageKeyPath: ReferenceWritableKeyPath<E, Self>
-//  ) -> T? {
-//    get {
-//      let field = instance[keyPath: storageKeyPath].field
-//      let data = instance.data[field]
-//      if let cacheKey = data as? CacheKey {
-//        return instance._transaction.entity(withKey: cacheKey.key)
-//
-//      } else if let dataDict = data as? [String: Any] {
-//        return instance._transaction.entity(withData: dataDict)
-//
-//      } else {
-//        return data as? T
-//      }
-//    }
-//    set {
-//      let field = instance[keyPath: storageKeyPath].field
-//
-//      switch newValue {
-//      case let entity as AnyCacheEntity:
-//        instance.data[field] = entity.data
-//
-//      default:
-//        break // TODO
-////        fatalError() // TODO
-//      }
-//    }
-//  }
-//
-//  public var projectedValue: CacheReference { self }
-//
-//  @available(*, unavailable,
-//      message: "This property wrapper can only be applied to CacheEntity objects."
-//  )
-//  public var wrappedValue: T? { get { fatalError() } set { fatalError() } }
-//}
+extension CacheEntity {
+  @propertyWrapper
+  public struct CacheRef<R> {
+
+    private let field: StaticString
+
+    public init(_ field: StaticString) {
+      self.field = field
+    }
+
+    public static subscript<E: CacheEntity, R>(
+      _enclosingInstance instance: E,
+      wrapped wrappedKeyPath: ReferenceWritableKeyPath<E, CacheReference<R>?>,
+      storage storageKeyPath: ReferenceWritableKeyPath<E, Self>
+    ) -> CacheReference<R>? {
+      get {
+        let field = instance[keyPath: storageKeyPath].field
+        let data = instance.data[field.description]
+
+        if let _ = data as? CacheKey {
+          fatalError()
+          //        return CacheReference.entity(instance._transaction.entity(withKey: cacheKey.key)) as? T
+
+        } else if let dataDict = data as? [String: Any] {
+          let entity: R = instance._transaction.entity(withData: dataDict) as! R
+          return CacheReference.entity(entity)
+          //        fatalError()
+
+        } else {
+          fatalError()
+        }
+      }
+      set {
+        //      let field = instance[keyPath: storageKeyPath].field
+        //
+        //      switch newValue {
+        //      case let reference as AnyCacheReference:
+        //        instance.data[field] = reference.cacheData
+        //
+        //      default:
+        //        break // TODO
+        ////        fatalError() // TODO
+        //      }
+      }
+    }
+
+    public var projectedValue: CacheRef { self }
+
+    @available(*, unavailable,
+    message: "This property wrapper can only be applied to CacheEntity objects."
+    )
+    public var wrappedValue: CacheReference<R>? { get { fatalError() } set { fatalError() } }
+  }
+}
+
 
 public protocol AnyCacheEntity {
   var data: [String: Any] { get }
