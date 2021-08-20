daafe63dcec40ab5761a10db92643a57049d97ee
Anthony Miller <AnthonyMDev@gmail.com>
2021-08-20 12:40:32 -0700

    CacheField getter will only replace the stored value for correct types

--- CodegenProposalFramework/Sources/CodegenProposalFramework/Common/MutableCache/CacheField.swift
+++ CodegenProposalFramework/Sources/CodegenProposalFramework/Common/MutableCache/CacheField.swift
@@ -13,14 +13,14 @@ public struct CacheField<T: Cacheable> {
     storage storageKeyPath: ReferenceWritableKeyPath<E, Self>
   ) -> T? {
     get {
-      let field = instance[keyPath: storageKeyPath].field
-      guard let data = instance.data[field.description] else {
+      let field = instance[keyPath: storageKeyPath].field.description
+      guard let data = instance.data[field] else {
         return nil
       }
 
       do {
         let value = try T.value(with: data, in: instance._transaction)
-        instance.mutateData { $0[field.description] = value }
+        replace(data: data, with: value, for: field, on: instance)
         return value
 
       } catch {
@@ -42,6 +42,27 @@ public struct CacheField<T: Cacheable> {
     }
   }
 
+  private static func replace(
+    data: Any,
+    with parsedValue: T,
+    for field: String,
+    on instance: AnyCacheObject)
+  {
+    /// Only need to do this for CacheEntity, Enums, and custom scalars.
+    /// DO NOT DO THIS when value is a CacheInterface ON a CacheInterface instance
+    /// For ScalarTypes, its redundant
+    /// TODO: Write tests for this.
+    switch (parsedValue) {
+    case is CacheEntity where !(data is CacheEntity),
+         is CacheInterface where instance is CacheInterface,
+         is CustomScalarType:
+      instance.mutateData { $0[field.description] = parsedValue }
+
+    case is CacheInterface, is ScalarType: break
+    default: break
+    }
+  }
+
   public var projectedValue: CacheField { self }
 
   @available(*, unavailable,
@@ -50,8 +71,15 @@ public struct CacheField<T: Cacheable> {
   public var wrappedValue: T? { get { fatalError() } set { fatalError() } }
 }
 
+/// A type that can be the value of a `@CacheField` property. In other words, a `Cacheable` type
+/// can be the value of a field on a `CacheEntity` or `CacheInterface`
+///
+/// # Conforming Types:
+/// - `CacheEntity`
+/// - `CacheInterface`
+/// - `ScalarType` (`String`, `Int`, `Bool`, `Float`)
+/// - `CustomScalarType`
+/// - `GraphQLEnum` (via `CustomScalarType`)
 public protocol Cacheable {
   static func value(with cacheData: Any, in transaction: CacheTransaction) throws -> Self
-//  init(cacheData: Any, transaction: CacheTransaction) throws
-  //  var cacheData: Any { get }
 }
